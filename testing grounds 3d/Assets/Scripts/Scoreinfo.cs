77611c26129c61559be9271a71ab2bb1c96653cd
BinglingBingus <tanominator@gmail.com>
2023-05-06 00:53:07 +0100

    Score resets to 0 after x time
    Need Ghost text for NEXT set of 4 letters
    will make higher scores easier and game less frustrating
    Need to make timer more forgiving

--- testing grounds 3d/Assets/Scripts/Scoreinfo.cs
+++ testing grounds 3d/Assets/Scripts/Scoreinfo.cs
@@ -10,26 +10,48 @@ public class Scoreinfo : MonoBehaviour
     public int          Score;
     public int          Scoring;
     public float        timercounter;
+    public TextMeshPro  ThisTextScore;
+    public TextMeshPro  ThisTextCountdown;
     // Start is called before the first frame update
 
 
     // Update is called once per frame
     void Start()
     {
+        SetTimer();
+    }
+    void FixedUpdate()
+    {
+
+        
+        IsTimerZero();
     }
 
     public void scoresystem(int Scoring)
     {
         Score += Scoring;
-        GetComponent<TMP_Text>().text = Score.ToString();
+        SetTimer();
+        ThisTextScore.text = Score.ToString();
         Debug.Log(Score);
     }
     
-    public void scoretimer()
+    public void IsTimerZero()
     {
-        //make timer smaller as score gets higher
-        // number1 = score /100 (0.1)
-        // number 2 = (score /10) - number1  // so 1 - 0.1 (0.9), 2 - 0.2 (1.8) need to minus from a flat
-        timercounter = Score / 10;
+        timercounter -= Time.deltaTime;
+        ThisTextCountdown.text = timercounter.ToString("0.0");
+        if(timercounter <= 0f)
+        {
+
+            Score = 0;
+            ThisTextScore.text = Score.ToString();
+            ThisTextCountdown.text = timercounter.ToString("0");
+            timercounter = 0;
+            
+        }
+    }
+
+    void SetTimer()
+    {
+        timercounter = 50f / Score;
     }
 }
